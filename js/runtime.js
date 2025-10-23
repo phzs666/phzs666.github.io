57e22ec12e62ab7fcec45b157f79aa7733d0766d
phzs <1933185230@qq.com>
2025-10-23 22:26:01 +0800

    Site updated: 2025-10-23 22:26:00

--- js/runtime.js
+++ js/runtime.js
@@ -27,13 +27,13 @@ function createtime() {
 
     currentTimeHtml =
         hnum < 18 && hnum >= 9
-            ? `<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F小屋-科研摸鱼中.svg' title='什么时候能够实现财富自由呀~'><br>
+            ? `<img class='boardsign' src='https://img.shields.io/badge/phzs%E5%B0%8F%E5%B1%8B-%E4%B8%8A%E7%8F%AD%E6%91%B8%E9%B1%BC%E4%B8%AD-green?color=green' title='什么时候能够实现财富自由呀~'>
          <div style="font-size:13px;font-weight:bold">
          本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 
          <i id="heartbeat" class='fas fa-heartbeat'></i> <br>
          旅行者 1 号当前距离地球 ${dis} 千米，约为 ${unit} 个天文单位 🚀
          </div>`
-            : `<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F小屋-下班休息啦.svg' title='下班了就该开开心心地玩耍~'><br>
+            : `<img class='boardsign' src='https://img.shields.io/badge/phzs%E5%B0%8F%E5%B1%8B-%E4%B8%8B%E7%8F%AD%E4%BC%91%E6%81%AF%E5%95%A6-green?color=green' title='下班了就该开开心心地玩耍~'>
          <div style="font-size:13px;font-weight:bold">
          本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 
          <i id="heartbeat" class='fas fa-heartbeat'></i> <br>
